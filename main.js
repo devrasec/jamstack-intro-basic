adcab27d26e4494a95c40423d2187b6453400267
Luis Contreras <one.tmac@gmail.com>
2020-05-14 11:19:38 +0200

    feat: my first jamstack site

---
+++ main.js
@@ -0,0 +1,21 @@
+const listRepos = async username => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then(res => res.json())
+    .catch(err => console.error(err));
+
+  const markup = repos.map(
+    repo => `
+      <li>
+        <a href="${repo.html_url}">${repo.name}</a>
+        (⭐ ${repo.stargazers_count})
+      </li>
+    `
+  ).join('');
+
+  const content = document.getElementById('content');
+  content.innerHTML = `<ul>${markup}</ul>`;
+}
+
+listRepos('devrasec');
